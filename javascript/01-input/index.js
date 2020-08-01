ce5ed2026f75983d3aa16897c17a75e6e3f86f23
Samuel Aditia <samueladitia95@gmail.com>
2020-08-01 18:49:06 +0700

    v2.4

--- javascript/01-input/index.js
+++ javascript/01-input/index.js
@@ -3,3 +3,13 @@ const sum = (...args) => {
 }
 
 console.log(sum(1, 2, 3, 4));
+
+function add(num1, num2) {
+  let total = 1;
+  for(let i = 1; i <= 10; i++) {
+    total += num1 + num2;
+  }
+  return total;
+}
+
+console.log(add(2, 3));
