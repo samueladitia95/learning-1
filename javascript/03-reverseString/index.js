9f155e4852a6d5fddb0f9c5391eea4cdda86a772
Samuel Aditia <samueladitia95@gmail.com>
2020-08-02 21:06:28 +0700

    v3.3

---
+++ javascript/03-reverseString/index.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+    if (!(typeof str === "string")) {
+        return "Invalid input, String only!"
+    }
+    if (str === "") {
+        return "Empty String!"
+    }
+    let reversed = str[0];
+    for (let i = 1; i < str.length; i++) {
+        reversed = str[i] + reversed;
+    }
+    return reversed.toLowerCase();
+}
+
+console.log(reverseString(""))
