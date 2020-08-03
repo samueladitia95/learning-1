1e83b370dc62431f801596707374e22dd69299ba
Samuel Aditia <samueladitia95@gmail.com>
2020-08-03 20:07:34 +0700

    v3.7

---
+++ hacktiv8Stuff/04-graduates/index.js
@@ -0,0 +1,30 @@
+function graduate(name, score, absents) {
+    // check valid name
+    if (typeof name !== "string") {
+        return "Invalid, not a valid name!";
+    }
+
+    // check valid score
+    if (score < 0 && score > 100) {
+        return "Invalid, not a valid score!";
+    }
+
+    // check valid absents
+    if (absents < 0) {
+        return "Invalid, not a valid absents number";
+    }
+
+    // check graduation status
+    if (score > 70 && absents < 5) {
+        return "Lulus";
+    } else {
+        return "Tidak Lulus";
+    }
+}
+
+let name = "John Smith";
+let score = 89;
+let absents = 2;
+let graduationStatus = graduate(name, score, absents);
+
+console.log(`${name} with a score of ${score} and total absents of ${absents} is declared: ${graduationStatus}`);
