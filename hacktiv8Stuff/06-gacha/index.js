92aafebbbffd146d8084a283e0aacee0662ef9fa
Samuel Aditia <samueladitia95@gmail.com>
2020-08-05 07:59:52 +0700

    v3.9

---
+++ hacktiv8Stuff/06-gacha/index.js
@@ -0,0 +1,18 @@
+var number = Math.ceil(Math.random() * 5);
+switch (number) {
+    case 1:
+        console.log("coba lagi ya");
+        break;
+    case 2:
+        console.log("selamat kamu mendapatkan kupon sebanyak 5");
+        break;
+    case 3:
+        console.log("selamat kamu mendapatkan kupon sebanyak 15");
+        break;
+    case 4:
+        console.log("selamat kamu mendapatkan kupon sebanyak 50");
+        break;
+    case 5:
+        console.log("WOW, kamu menang jackpot! Selamat!!");
+        break;
+}
