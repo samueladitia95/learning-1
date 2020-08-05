2599199395fe9d369e8dde9e1f1a493ca82055bc
Samuel Aditia <samueladitia95@gmail.com>
2020-08-05 20:16:30 +0700

    v3.11

---
+++ hacktiv8Stuff/09-compareNum/index.js
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = 5;
+
+if (num1 === num2) {
+    console.log(-1);
+} else if (num1 > num2) {
+    console.log(false);
+} else {
+    console.log(true)
+}
---
+++ hacktiv8Stuff/10-pairOfLargestNum/index.js
@@ -0,0 +1,11 @@
+let angka = 7991829356169341;
+let angkaString = angka.toString();
+let top = Number(angkaString.substring(0, 2));
+
+for (let i = 0; i < angkaString.length - 2; i++) {
+    let topTemp = Number(angkaString.substring(i + 1, i + 3))
+    if (topTemp > top) {
+        top = topTemp;
+    }
+}
+console.log(top);
