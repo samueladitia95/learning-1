32590eb09929b1fa1c90979da5e5644861316bc2
Samuel Aditia <samueladitia95@gmail.com>
2020-08-06 14:13:09 +0700

    v3.15

--- hacktiv8Stuff/13-triangle/index.js
+++ hacktiv8Stuff/13-triangle/index.js
@@ -1,13 +1,13 @@
-let num = 8;
+let num = 10;
 
 for (let i = 1; i <= num; i++) {
     let printed = "";
     for (let j = 1; j <= (2 * num - 1); j++) {
         if (j >= num - i + 1 && j <= num + i - 1) {
-            if (j % 2 == 0) {
-                printed += "O";
+            if (i % 2 === 0) {
+                (j % 2 === 0) ? printed += "X" : printed += "O"
             } else {
-                printed += "X"
+                (j % 2 === 0) ? printed += "O" : printed += "X"
             }
         } else {
             printed += " "
