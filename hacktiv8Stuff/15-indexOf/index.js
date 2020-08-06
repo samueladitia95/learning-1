a18b62c2c129e3884b532c8bba82f93081252e82
Samuel Aditia <samueladitia95@gmail.com>
2020-08-06 10:36:18 +0700

    v3.13

---
+++ hacktiv8Stuff/15-indexOf/index.js
@@ -0,0 +1,14 @@
+let input1 = "hello my name is john wick, and i love my dog so much that i hurt any one that hurt him!";
+let input2 = "him";
+let status =  false;
+
+for (let i = 0; i < input1.length; i++) {
+    if (input1.substring(i, i + (input2.length))  === input2) {
+        console.log(i);
+        status = true
+    }
+}
+
+if (!status) {
+    console.log(-1)
+}
