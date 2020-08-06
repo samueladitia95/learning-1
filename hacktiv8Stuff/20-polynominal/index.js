99f53d69c4765a6dddda17fdee56fdd5d5d732ec
Samuel Aditia <samueladitia95@gmail.com>
2020-08-06 18:37:04 +0700

    v3.17

--- hacktiv8Stuff/20-polynominal/index.js
+++ hacktiv8Stuff/20-polynominal/index.js
@@ -5,21 +5,45 @@
 // console.log(polynominal('a'))  // a
 
 function polynominal (rumus) {
-    let type = [];
+    // split into array
+    let split = [];
     let tempString = "";
     for (let i = 0; i < rumus.length; i++) {
         if (rumus[i] !== "+") {
             tempString += rumus[i];
         } else {
-            type.push(tempString);
+            split.push(tempString);
             tempString = "";
         }
         if (i === rumus.length - 1) {
-            type.push(tempString)
+            split.push(tempString)
         }
     }
-    return type;
+
+    // group array into type and count the number of each type
+    let type = {};
+    for (let i = 0; i < split.length; i++) {
+        let tempString = "";
+        let tempNum = 1;
+        for (let j = 0; j < split[i].length; j++) {
+            if (!Number(split[i][j])) {
+                tempString += split[i][j];
+            } else {
+                tempNum = Number(split[i][j]);
+            }
+        }
+        if (!type[tempString]) {
+            type[tempString] = 0;
+        }
+        type[tempString] += tempNum;
+    }
+    return type
 }
 
-console.log(polynominal('a+2ab+3b+4c+5bc')) // ['a', '2ab', '3b', '4c', '5bc']
+console.log(polynominal('a+2ab+3b+4c+5bc+4ab')) // ['a', '2ab', '3b', '4c', '5bc']
 console.log(polynominal('a+2a+3a+4a+5a')) // ['a', '2a', '3a', '4a', '5a']
+console.log(polynominal('a+2ab+3b+4c+5bc')) // a+2ab+3b+5bc+4c
+console.log(polynominal('a+2a+3a+4a+5a')) // 15a
+console.log(polynominal('a+2a+3b+4a+5a+4')) // 4+12a+3b
+console.log(polynominal('1')) // 1
+console.log(polynominal('a'))  // a
